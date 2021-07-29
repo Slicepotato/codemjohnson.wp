6431291512a370be8092cdfab5c47cb917161186
Michael Johnson <m.ianjohnson@gmail.com>
2021-07-28 22:45:18 -0400

    revise  functions - rest api

--- headless/functions.php
+++ headless/functions.php
@@ -1,21 +1,26 @@
 <?php
-function expose_navigation($request) {
-    $id = $request['id'];
-    return wp_get_nav_menu_items($id);
-  }
-  
-  /**
-   * Exposes under /navigation/{id} the menu items in the wp-json api
-   *
-   * @return void
-   */
-  function expose_navigation_to_rest() {
-    register_rest_route( 'wp/v2', '/menu/(?P<id>\d+)', [
+function wp_menu_route() {
+    $menuLocations = get_nav_menu_locations(); // Get nav locations set in theme, usually functions.php)
+    return $menuLocations;
+    }
+
+    add_action( 'rest_api_init', function () {
+        register_rest_route( 'wp/v2', '/menu/', array(
         'methods' => 'GET',
-        'callback' => 'expose_navigation'
-      ]
-    );
-  }
-  
-  add_action('rest_api_init', 'expose_navigation_to_rest');
+        'callback' => 'wp_menu_route',
+    ) );
+} );
+
+function wp_menu_single($data) {
+    $menuID = $data['id']; // Get the menu from the ID
+    $primaryNav = wp_get_nav_menu_items($menuID); // Get the array of wp objects, the nav items for our queried location.
+    return $primaryNav;
+    }
+
+    add_action( 'rest_api_init', function () {
+        register_rest_route( 'wp/v2', '/menu/(?P<id>\d+)', array(
+        'methods' => 'GET',
+        'callback' => 'wp_menu_single',
+    ) );
+} );
 ?>
